528eb0cafecc2ba3d15cc9e01c1e19bdfd19aa52
1111 Technologies <rpranaya.2000@gmail.com>
2021-11-01 18:59:05 +0800

    change link

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const socket = require("socket.io")
 
 const io = socket(server, {
     cors: {
-      origin: "https://limedraw.herokuapp.com/",
+      origin: "https://limedrawio.herokuapp.com/",
       methods: ["GET", "POST"]
     }});
 
