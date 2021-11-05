df9f9340daadb7c9ac2243885632076eb1c23e55
1111 Technologies <rpranaya.2000@gmail.com>
2021-11-05 11:57:52 +0800

    major changes

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const http = require("http");
 const app = express();
 const server = http.createServer(app);
 const socket = require("socket.io")
+const cors = require('cors')
+app.use(express.json());
+app.use(cors())
 
 const io = socket(server, {
     cors: {
@@ -39,33 +42,37 @@ io.on('connection', socket => {
     })
     
     socket.on('join room', obj => {
-        io.emit("welcome", {roomID: obj.roomID, user: obj.user});
-        
+
         if (rooms[obj.roomID]) {
-            const length = rooms[obj.roomID].length;
+            if (rooms[obj.roomID].isStarting){
+                socket.emit("room started");
+                return;
+            }
+            const length = rooms[obj.roomID].users.length;
 
             if (length === 6) {
                 socket.emit("room full");
                 return;
             }
 
-            rooms[obj.roomID].push({id: socket.id, user: obj.user});
+            rooms[obj.roomID].users.push({id: socket.id, user: obj.user});
             starts[obj.roomID].push({id: socket.id, start: true});
             userSnapshot[obj.roomID].push({id: socket.id, user: obj.user});
 
         } else {
             userSnapshot[obj.roomID] = [{id: socket.id, user: obj.user}];
             starts[obj.roomID]=[{id: socket.id, start: true}];
-            rooms[obj.roomID] = [{id: socket.id, user: obj.user}];
+            rooms[obj.roomID] = {type: obj.roomType, isStarting: false, users :[{id: socket.id, user: obj.user}]};
+            
         }
 
         socketToRoom[socket.id] = obj.roomID;
-
-        io.emit("all users", {roomID: obj.roomID, usersInThisRoom: rooms[obj.roomID], userSnapshot:  userSnapshot[obj.roomID]});
-        const usersExcludeinRoom = rooms[obj.roomID].filter(obj => obj.id !== socket.id);
+        io.emit("welcome", {roomID: obj.roomID, user: obj.user});
+        io.emit("all users", {roomID: obj.roomID, usersInThisRoom: rooms[obj.roomID].users, userSnapshot:  userSnapshot[obj.roomID]});
+        const usersExcludeinRoom = rooms[obj.roomID].users.filter(obj => obj.id !== socket.id);
         
         // socket.broadcast.emit('user-connected', {roomID: obj.roomID, user: socket.id})
-        io.emit("peers",  {roomID: obj.roomID, user: rooms[obj.roomID]});
+        io.emit("peers",  {roomID: obj.roomID, user: rooms[obj.roomID].users});
     });
 
     socket.on("canvas-data", data =>{
@@ -77,7 +84,7 @@ io.on('connection', socket => {
     })
 
     socket.on("add point", data =>{
-        rooms[data.roomID] = data.user;
+        rooms[data.roomID].users = data.user;
 
         if (answers[data.roomID]){
             answers[data.roomID].push(data.id)
@@ -85,7 +92,7 @@ io.on('connection', socket => {
             answers[data.roomID] = [data.id]
         }
 
-        if(answers[data.roomID].length === rooms[data.roomID].length - 1){
+        if(answers[data.roomID].length === rooms[data.roomID].users.length - 1){
             answers[data.roomID] = []
             io.emit("all answer", data.roomID)
         }
@@ -95,13 +102,14 @@ io.on('connection', socket => {
             starts[data.roomID].forEach(function(part, index) {
                 part.start = false;
               });
+              rooms[data.roomID].isStarting = false
             io.emit("game over", data )
         }
         socket.broadcast.emit("add point", data)
     })
 
     socket.on("minus point", data =>{
-        rooms[data.roomID] = data.user;
+        rooms[data.roomID].users = data.user;
         socket.broadcast.emit("minus point", data)
     })
 
@@ -111,6 +119,7 @@ io.on('connection', socket => {
         if(idx !== -1){
             socket.emit("not ready", data)
         } else {
+            rooms[data.roomID].isStarting = true
             socket.broadcast.emit("start", data)
         }
     })
@@ -140,9 +149,9 @@ io.on('connection', socket => {
     })
 
    socket.on("mute", data=>{
-    var idx = rooms[data.roomID].findIndex((obj => obj.id == data.id));
+    var idx = rooms[data.roomID].users.findIndex((obj => obj.id == data.id));
     if(idx>-1){
-        rooms[data.roomID][idx].user.mute = true
+        rooms[data.roomID].users[idx].user.mute = true
     }
 
     socket.broadcast.emit("mute", data)
@@ -151,13 +160,43 @@ io.on('connection', socket => {
    })
 
    socket.on("unmute", data=>{
-    var idx = rooms[data.roomID].findIndex((obj => obj.id == data.id));
+    var idx = rooms[data.roomID].users.findIndex((obj => obj.id == data.id));
     if(idx>-1){
-        rooms[data.roomID][idx].user.mute = false
+        rooms[data.roomID].users[idx].user.mute = false
     }
 
     socket.broadcast.emit("unmute", data)
    })
+
+   socket.on('logout', ()=>{
+    const roomID = socketToRoom[socket.id];
+    let room = rooms[roomID];
+    let snapshots = userSnapshot[roomID]
+    let start = starts[roomID]
+    let answer = answers[roomID]
+    if (room) {
+        var activeUser = room.users
+        var self = room.users
+        activeUser = activeUser.filter(obj => obj.id !== socket.id);
+        self = self.filter(obj => obj.id == socket.id);
+        rooms[roomID].users = activeUser;
+
+        if (activeUser.length < 1){
+            delete rooms[roomID]
+        }
+
+        snapshots = snapshots.filter(obj => obj.id !== socket.id)
+        userSnapshot[roomID] = snapshots
+        start = start.filter(obj => obj.id !== socket.id)
+        if (answer)  {
+            answer= answer.filter(obj => obj !== socket.id)
+            answers[roomID] = answer
+        }
+        starts[roomID] = start
+        socket.broadcast.emit("disconnected", {roomID: roomID, usersInThisRoom: activeUser, userdisconnect : self, snapShots: snapshots});
+        // socket.broadcast.emit("peers", activeUser );
+        }
+    });
 
 
     socket.on('disconnect', () => {
@@ -167,11 +206,16 @@ io.on('connection', socket => {
         let start = starts[roomID]
         let answer = answers[roomID]
         if (room) {
-            var activeUser = room
-            var self = room
+            var activeUser = room.users
+            var self = room.users
             activeUser = activeUser.filter(obj => obj.id !== socket.id);
             self = self.filter(obj => obj.id == socket.id);
-            rooms[roomID] = activeUser;
+            rooms[roomID].users = activeUser;
+
+            if (activeUser.length < 1){
+                delete rooms[roomID]
+            }
+
             snapshots = snapshots.filter(obj => obj.id !== socket.id)
             userSnapshot[roomID] = snapshots
             start = start.filter(obj => obj.id !== socket.id)
@@ -192,6 +236,26 @@ io.on('connection', socket => {
     // socket.emit('welcome', 'Welcome to the party!')
     // socket.broadcast.emit('welcome', 'usera has joined the party')
 
+});
+
+
+app.post("/available-group",async (req,res, next)=>{
+    var roomidTemp = req.body.roomID
+    var availRoom = Object.keys(rooms)
+                       .filter(roomid => rooms[roomid].type === 'public' && rooms[roomid].users.length <=5 && !rooms[roomid].isStarting)
+                       .map(roomid => roomid);
+    
+    if (availRoom.length !== 0){
+        roomidTemp = availRoom[0]
+    }
+
+    try{
+        res.send(roomidTemp);
+
+    }catch(e){
+        console.log(e)
+        res.sendStatus(500);
+    }
 });
 
 server.listen(process.env.PORT || 5000, () => console.log('server is running on port 5000'));
